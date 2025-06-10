a5bc85f1fe4df13b75d95a595d254e4ade64955e
Shubham Nikam <114049680+shubhamsnikam@users.noreply.github.com>
2025-06-10 21:54:03 +0530

    Update Customer.js

--- models/Customer.js
+++ models/Customer.js
@@ -1,11 +1,37 @@
 const mongoose = require('mongoose');
 
 const customerSchema = new mongoose.Schema({
-  name: { type: String, required: true },
-  mobile: { type: String, required: true },
-  email: String,
-  address: String,
-  createdAt: { type: Date, default: Date.now }
+    name: {
+        type: String,
+        required: true
+    },
+    mobile: {
+        type: String,
+        required: true
+    },
+    // email: String, // Removed - You can comment out or delete this line
+    address: {
+        type: String
+    },
+    city: { // Added city field
+        type: String
+    },
+    billNumber: { // Added billNumber field
+        type: String
+    },
+    warrantyDateFrom: { // Added warrantyDateFrom field
+        type: Date
+    },
+    warrantyDateTo: { // Added warrantyDateTo field
+        type: Date
+    },
+    modelName: { // Added modelName field
+        type: String
+    },
+    createdAt: {
+        type: Date,
+        default: Date.now
+    }
 });
 
 module.exports = mongoose.model('Customer', customerSchema);
