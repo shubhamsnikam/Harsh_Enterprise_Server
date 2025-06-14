2e625cb7836e61780e71a3dd58762d93f3417e4b
Shubham Nikam <114049680+shubhamsnikam@users.noreply.github.com>
2025-06-14 12:59:59 +0530

    Update server.js

--- server.js
+++ server.js
@@ -26,3 +26,4 @@ mongoose.connect(process.env.MONGO_URI, {
   console.log('MongoDB connected');
   app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
 }).catch(err => console.error(err));
+ 
